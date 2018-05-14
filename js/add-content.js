a6eaf16ec63988a46599dc9f796aca6c13469c83
Usong Cho <silvermouse4012@gmail.com>
2018-05-13 20:28:09 -0700

    initial commit

--- js/add-content.js
+++ js/add-content.js
@@ -1,15 +1,43 @@
-var today = new Date();
-var hourNow = today.getHours();
-var greeting;
+// var today = new Date();
+// var hourNow = today.getHours();
+// var greeting;
 
-if (hourNow > 18) {
-    greeting = 'Good evening!';
-} else if (hourNow > 12) {
-    greeting = 'Good afternoon!';
-} else if (hourNow > 0) {
-    greeting = 'Good morning!';
+// if (hourNow > 18) {
+//     greeting = 'Good evening!';
+// } else if (hourNow > 12) {
+//     greeting = 'Good afternoon!';
+// } else if (hourNow > 0) {
+//     greeting = 'Good morning!';
+// } else {
+//     greeting = 'Welcome!';
+// }
+
+// document.write('<h3>' + greeting + '</h3>');
+
+
+
+
+
+
+
+
+
+
+
+
+
+let today = new Date();
+let timeNow = today.getHours();
+let greetings;
+
+if ( timeNow >= 18 ) {
+    greetings = 'Good Evening';
+} else if ( timenow >= 12 ) {
+    greetings = 'Good Morning';
+} else if ( timenow >= 0 ) {
+    greetings = 'Good Morning';
 } else {
-    greeting = 'Welcome!';
+    greeting = "Welcome";
 }
 
-document.write('<h3>' + greeting + '</h3>');
+document.write('<h3>' + greetings + '</h3>');
